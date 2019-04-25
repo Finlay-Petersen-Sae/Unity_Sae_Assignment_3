f4e0d98f040f2816e26fe2476811ac54af79b5c4
Finlay-Petersen-Sae <1015492@student.sae.edu.au>
2019-04-25 12:47:33 +1000

    Gad170 Project without commenting
    complete Gad170 task 3 without commenting

---
+++ GAD170_3/Assets/Scripts/GoalPointCheckListItem.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Tracks number of shots up to a target amount. Is told directly by Hoop via Unity Event. 
+/// </summary>
+public class GoalPointCheckListItem : CheckListItem
+{
+    public int numberOfRequiredCheckpoints;
+    public int numberOfCheckpointsScored;
+
+    public override bool IsComplete { get { return numberOfCheckpointsScored == numberOfRequiredCheckpoints; } }
+
+    public override float GetProgress()
+    {
+        return (float)numberOfCheckpointsScored / (float)numberOfRequiredCheckpoints;
+    }
+
+    public override string GetStatusReadout()
+    {
+        return numberOfCheckpointsScored.ToString() + " / " + numberOfRequiredCheckpoints.ToString();
+    }
+
+    public override string GetTaskReadout()
+    {
+        return "Find the Checkpoints";
+    }
+
+    public void OnCheckPointPassed()
+    {
+        if (numberOfCheckpointsScored < numberOfRequiredCheckpoints)
+        {
+            var ourData = new GameEvents.CheckListItemChangedData();
+            ourData.item = this;
+            ourData.previousItemProgress = GetProgress();
+
+            numberOfCheckpointsScored++;
+
+            GameEvents.InvokeCheckListItemChanged(ourData);
+        }
+    }
+}
---
+++ GAD170_3/Assets/Scripts/NerfDartCheckListItem.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NerfDartCheckListItem : CheckListItem
+{
+    public List<GameObject> Dragons = new List<GameObject>();
+    public int numberOfDragonsHit;
+
+    public override bool IsComplete { get { return Dragons.Count == 0; } }
+
+    public override float GetProgress()
+    {
+        return (float)numberOfDragonsHit / (float)Dragons.Count;
+    }
+
+    public override string GetStatusReadout()
+    {
+        return (numberOfDragonsHit - Dragons.Count) + " / " + numberOfDragonsHit.ToString() ;
+    }
+
+    public override string GetTaskReadout()
+    {
+        return "Shoot some Dragons";
+    }
+
+    public void Start()
+    {
+        numberOfDragonsHit = Dragons.Count;
+    }
+
+    public void OnDragonHit()
+    {
+        if (numberOfDragonsHit < Dragons.Count)
+        {
+            var ourData = new GameEvents.CheckListItemChangedData();
+            ourData.item = this;
+            ourData.previousItemProgress = GetProgress();
+
+            numberOfDragonsHit++;
+
+            GameEvents.InvokeCheckListItemChanged(ourData);
+        }
+    }
+    private void GameEvents_OnObjectReset(GameEvents.PhysicsInteractionData data)//change on object reset
+    {
+        Debug.Log("Get To hear?");
+        // Here we are checking first if we have not dropped the ball, and that the object that what has entered reset world trigger
+        // in this case our ball has a rigidbody and has the take of "Ball".
+        if (!IsComplete && data != null && data.other.tag == "NerfDart")// or data.instigator
+        {
+            Debug.Log("Get To hear");
+            // We can use the keyword var to hold any sort of data, i.e. a var ourData we could put anythin into it, a bool, a string, an int etc.
+            // The downside is because it can be everything Unity has to assign the most amount of memory it can to accomodate it, so use var's wisely especially.
+            // in big projects. If I didn't want to use a var I could write it as GameEvents.CheckListItemChangedData ourData = new GameEvents.CheckListItemChangedData(); .
+            // For this case we are creating a new instance of the class CheckListItemChangedData and storing it in our var  
+            var ourData = new GameEvents.CheckListItemChangedData();
+            // I then take ourData and get the item variable and assign the instance of this class to it, using the 'this' keyword.
+            ourData.item = this;
+            // From there I can set the previousItemProgress Variable of our data to our current progress in this case it return 1 as we have dropped the ball over the edge.
+            // i.e. from 0 being false, to 1 being true, so the previous item progress was it hadn't been dropped over the edge to now it has been dropped over the edge.
+            Dragons.Remove(data.instigator);
+            ourData.previousItemProgress = GetProgress();
+            // Since the ball has now gone over the edge we want to se the hasDroppedBall bool to true, so we can't do this function again as the task is complete.
+           
+            // We then tell the game events to invoke the CheckListItemChanged event and pass in our data.
+            // this will take our data and up the tasks completed in the UI.
+            GameEvents.InvokeCheckListItemChanged(ourData);
+
+        }
+    }
+
+    // OnEnable is called whenever this script is turned on/off.
+    // It is the first function to be called when Unity starts and the script is active.
+    // In this case, we want to subscribe our GameEvents_OnObjectReset to the GameEvents.OnObjectReset event.
+    // We do this by using the += syntax.
+    // This is so when the GameEvents.OnObjectReset event is triggered our function will also get called.
+    private void OnEnable()
+    {
+        GameEvents.OnImportantPhysicsCollision += GameEvents_OnObjectReset;
+    }
+
+    // Conversley OnDisable is the last function to be called on a script.
+    // In this function we can dictate what happens when we disable this script.
+    // In our case we want to unsubscribe from the GameEvents.OnObjectReset Event.
+    // If we don't do this, Unity will flip out as it is expecting there to be a function it can call but it can't as
+    // we have disabled the script. To unsubscribe from the event we use the -= syntax.
+    private void OnDisable()
+    {
+        GameEvents.OnImportantPhysicsCollision -= GameEvents_OnObjectReset;
+    }
+}
+
